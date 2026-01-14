52eeec7fea92ca83954de8798a9473d36a98e6ce
Parisa Reza <rezaparisa5@gmail.com>
2026-01-15 04:14:15 +0600

    to do: validator and error handling middlewares

--- server/src/middlewares/validator.ts
+++
@@ -1,9 +0,0 @@
-import { ZodSchema } from "zod";
-import { NextFunction, Request, Response } from "express";
-
-export const validateRequestBody = (schema: ZodSchema) => {
-  return (req: Request, _res: Response, next: NextFunction) => {
-    schema.parse(req.body);
-    next();
-  };
-};
