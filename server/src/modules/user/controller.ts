88710bbe2ccba91af753dadf652d8c248ce26b33
Parisa Reza <rezaparisa5@gmail.com>
2026-01-26 11:57:15 +0600

    added user service for creating a user

--- server/src/modules/user/controller.ts
+++ server/src/modules/user/controller.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from "express";
+import { userService } from ".";
 
-export const createUser = (_req: Request, _res: Response) => {
+export const createUser = async  (req: Request, res: Response) => {
   // Logic to create a user
-   console.log("user created",_req.body);
+   const newUser = await userService.createUser(req.body);
 
-
-  _res.status(201).json({ message: "User created successfully" });
+  res.status(201).json({ newUser });
 }
---
+++ server/src/modules/user/service.ts
@@ -0,0 +1,27 @@
+import { z } from "zod";
+import bcrypt from "bcrypt";
+
+import { CreateUserSchema } from "@/schemas";
+import { UserModel } from "./model";
+import { envConfig } from "@/config";
+
+export const getHashedPassword = async (password: string) =>
+  bcrypt.hash(password, envConfig.BCRYPT_SALT_ROUNDS);
+
+export const createUser = async (
+  userPayload: z.infer<typeof CreateUserSchema>,
+) => {
+  const hashedPassword = await getHashedPassword(userPayload.password);
+  const newUser = await UserModel.create({
+    name: userPayload.name,
+    email: userPayload.email,
+    passwordHash: hashedPassword,
+    role: 'shop-keeper',
+    isDraft: true,
+  });
+  return newUser; // returns newUser to controller
+};
+
+
+//Request → admin/user.route.ts → validator.ts (CreateUserSchema) → 
+// controller.ts → service.ts → model.ts → DB
