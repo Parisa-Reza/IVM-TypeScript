c2f4f89ed8d43ec4f8cd6b3e903cc4e8aa8ac607
Parisa Reza <rezaparisa5@gmail.com>
2026-01-26 08:49:20 +0600

    added health check

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,10 +1,16 @@
 import { Express } from "express";
 
 import adminRouter from "./admin";
+import baseRouter from "./health";
 
  const configureRouters = (app: Express) => {
+app.use("/api/", baseRouter);
   app.use("/api/admin", adminRouter);
 }
 
 export default configureRouters;
 // here flow will go to admin/index.ts then admin/index.ts will redirect to user.route.ts
+
+//  app.ts-> routes/index.ts -> admin/index.ts ->user.route.ts
+
+// app.ts -> routes/index.ts -> health.ts
